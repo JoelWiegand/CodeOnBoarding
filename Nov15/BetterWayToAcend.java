a0643d3a4c8a18848107e351a745c6178ddfa4b9
JoelWiegand <16wiegandjr@gmail.com>
2021-11-18 09:15:02 -0600

    code new way

---
+++ Nov15/BetterWayToAcend.java
@@ -0,0 +1,28 @@
+public class BetterWayToAcend {
+    //code to create a string of ascending numbers instead of spliting, allows for a easier solution to inlude 99100
+    //idea based off of https://www.geeksforgeeks.org/consecutive-sequenced-numbers-in-a-string/
+    public static void main(String[] args) {
+        System.err.println( ascending("442443"));
+    }
+    public static boolean ascending(String str) {
+        long strLength=str.length();
+
+        for(int i =0; i<strLength/2;i++){
+            //half of length because then it can be two numbers that take up half of str each
+            Long tempNum;//var to hold current number
+            tempNum=Long.parseLong(str.substring(0, i+1));//get first number
+            String correctString=String.valueOf(tempNum);//the created string to match to input string
+            while(correctString.length()<strLength){
+                //create a correctString of size at least str.length()
+                tempNum++;//get next number
+                correctString+=String.valueOf(tempNum);//value of is safer than implict converstion    
+            }
+            System.err.println(correctString);
+            if(correctString.equals(str)){
+                //if the strings match it is what is should be
+                return true;
+            }
+        }
+        return false;
+    }
+}
